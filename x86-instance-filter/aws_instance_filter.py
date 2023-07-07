96ba41b9c675bd2d3ae84aa4ea8f9b4643e195a2
limjunho <jewel7492@gmail.com>
2023-07-07 19:22:52 +0900

    [REFACTOR] Use aws api to query the createable x86 instance of us-west-2

--- x86-instance-filter/aws_instance_filter.py
+++ x86-instance-filter/aws_instance_filter.py
@@ -1,20 +1,38 @@
 import os
 import pandas as pd
 
-csv_file = os.path.join(os.path.dirname(__file__), "AWS - ec2 price(us-west-2, 23.07.07).csv")
+import boto3
 
-columns = ["API Name", "Physical Processor", "On Demand"]
+# EC2 클라이언트 생성
+ec2_client = boto3.client('ec2', region_name = 'us-west-2')
 
-df = pd.read_csv(csv_file, usecols=columns)
+createable_x86_64_instances = []
+createable_arm64_instances = []
+unsupported_instances = []
+next_token = None
 
-# Remove instances that are not available in us-west-2
-df = df[df["On Demand"] != "unavailable"]
+while True:
+    # 인스턴스 타입 정보 가져오기
+    if next_token:
+        response = ec2_client.describe_instance_types(NextToken=next_token)
+    else:
+        response = ec2_client.describe_instance_types()
 
-# Remove instances with ARM architecture (Graviton)
-df = df[~df["Physical Processor"].str.contains("Graviton")]
+    # 현재 페이지의 인스턴스 유형 이름 추가
+    for instance_type in response['InstanceTypes']:
+        if instance_type['CurrentGeneration']:
+            architectures = instance_type['ProcessorInfo']['SupportedArchitectures']
+            if 'x86_64' in architectures:
+                createable_x86_64_instances.append(instance_type['InstanceType'])
 
-# Select only the "API Name" column
-df = df["API Name"]
+    # 다음 페이지 토큰 설정
+    next_token = response.get('NextToken', None)
+
+    # 다음 페이지 토큰이 없으면 종료
+    if not next_token:
+        break
+
+df = pd.DataFrame(createable_x86_64_instances, columns=['InstanceType'])
 
 output_csv_file = f"{os.path.dirname(__file__)}/AWS x86 instances(us-west-2, 23.07.07).csv"
-df.to_csv(output_csv_file, index=False, header=False)
+df.to_csv(output_csv_file, index=False)
