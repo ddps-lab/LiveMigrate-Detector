aad830dbaab8680186629e01b35a88ebad54a1e0
limjunho <jewel7492@gmail.com>
2023-07-07 19:42:03 +0900

    [ADD] Upload CPUID data to S3

--- get_cpuid/collect_start.sh
+++ get_cpuid/collect_start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# 프로세스 실행 명령어
 /home/ubuntu/get_cpuid/main
 
-# 프로세스 실행이 완료될 때까지 대기
 wait
 
-# 프로세스 실행이 완료되면 시스템 셧다운
+aws s3 cp /home/ubuntu/get_cpuid/cpuid.csv s3://us-west-2-cpuid-x86/$(curl http://169.254.169.254/latest/meta-data/instance-type).csv
+
+wait
+
 sudo shutdown -h now
