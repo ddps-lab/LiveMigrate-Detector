c7b20464c03f149bab2474ec51d1990ce4fe1ea0
limjunho <jewel7492@gmail.com>
2023-07-11 10:42:59 +0900

    [ADD] dataprocessing - groupby isa

---
+++ data-processing/GroupbyISA.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+import copy
+
+import GspreadUtils
+import ISA_h
+
+df = GspreadUtils.read_CPU_Feature_Visualization('us-west-2 x86 isa set')
+
+# Extract instance types with the same CPU ISA
+columns = copy.deepcopy(ISA_h.ISAs)
+columns.insert(0, 'instance groups')
+
+groupList = []
+flagList = []
+grouped = df.groupby(ISA_h.ISAs)
+i = 0
+
+df_new = pd.DataFrame(columns=columns)
+
+for features, group in grouped:
+    i += 1
+    instanceTypes = ', '.join(group['instancetype'].tolist())
+
+    eachFlag = group[ISA_h.ISAs]
+    row = eachFlag.iloc[0]
+    row = row.to_frame().T
+    row.insert(0, 'instance groups', instanceTypes)
+
+    df_new = pd.concat([df_new, row], ignore_index=True)
+
+GspreadUtils.write_CPU_Feature_Visualization('groupby isa', df_new)
