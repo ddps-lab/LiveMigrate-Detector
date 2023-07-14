1f7a2c6c5831b607bfbe599bda89075183eb3a08
limjunho <jewel7492@gmail.com>
2023-07-14 14:10:02 +0900

    [REFACTOR] Change duplicate usage variable to constant

--- data-processing/GroupbyISA.py
+++ data-processing/GroupbyISA.py
@@ -7,13 +7,15 @@ import ISA_h
 
 df = GspreadUtils.read_gspread('us-west-2 x86 isa set')
 
+ISAs = ISA_h.ISAs
+
 # Extract instance types with the same CPU ISA
-columns = copy.deepcopy(ISA_h.ISAs)
+columns = copy.deepcopy(ISAs)
 columns.insert(0, 'instance groups')
 
 groupList = []
 flagList = []
-grouped = df.groupby(ISA_h.ISAs)
+grouped = df.groupby(ISAs)
 i = 0
 
 df_new = pd.DataFrame(columns=columns)
@@ -22,7 +24,7 @@ for features, group in grouped:
     i += 1
     instanceTypes = ', '.join(group['instancetype'].tolist())
 
-    eachFlag = group[ISA_h.ISAs]
+    eachFlag = group[ISAs]
     row = eachFlag.iloc[0]
     row = row.to_frame().T
     row.insert(0, 'instance groups', instanceTypes)
