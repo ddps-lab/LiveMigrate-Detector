3d340ab320301c649c355c9453cec24f972f035d
limjunho <jewel7492@gmail.com>
2023-07-19 20:40:33 +0900

    [REFACTOR] remove un-used code

--- data-processing/SupersetVisualization(Digraph).py
+++ data-processing/SupersetVisualization(Digraph).py
@@ -1,8 +1,7 @@
 import GspreadUtils
 import Transferable
 
-df = GspreadUtils.read_gspread('groupby isa')
-featureGroups = df['instance groups'].tolist()
+df = GspreadUtils.read_gspread('mat_mul(r4.large)')
 df = df.drop('instance groups', axis=1)
 
 GROUP_NUMBER = df.shape[0]
