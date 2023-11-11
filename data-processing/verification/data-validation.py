a95428b7ac43e271581f84b7a4ed2f13967c58f2
limjunho <jewel7492@gmail.com>
2023-11-11 19:00:08 +0900

    [ADD] validation of CRIU cpu compatibility check

--- data-processing/verification/data-validation.py
+++ data-processing/verification/data-validation.py
@@ -21,10 +21,10 @@ ec2_resource = boto3.resource('ec2', region_name='us-west-2')
 s3_client = boto3.client('s3')
 
 bucket_name = 'migration-compatibility'
-prefix = 'func_tracking/redis/'
+prefix = 'func_tracking/rdseed/'
 
 def readCSV():
-    df = pd.read_csv(f'{root_path}/data-processing/verification/redis.csv')
+    df = pd.read_csv(f'{root_path}/data-processing/verification/rdseed.csv')
     migration_success = df[df['migration_success'] == True]
     migration_failed = df[df['migration_success'] == False]
 
@@ -33,6 +33,73 @@ def readCSV():
     migration_failed = migration_failed[selected_columns]
 
     return migration_success, migration_failed
+
+
+def criu_cpu_info_check():
+    recall = 0
+
+    instances =["m5a.large", "m5a.2xlarge", "m5a.8xlarge", "c5a.large", "c6a.large", "m4.large", "h1.2xlarge", "x1e.xlarge", "r4.large", "i3.large", "c5a.24xlarge", "c6a.24xlarge", "c4.8xlarge", "h1.8xlarge", "h1.16xlarge", "x1e.8xlarge", "m4.16xlarge", "r4.8xlarge", "r4.16xlarge", "c6i.large", "c5.large", "m5n.large", "m5.large", "c6i.16xlarge", "c5d.9xlarge", "m5zn.6xlarge", "c5.9xlarge"]
+
+    df = pd.read_csv(f'{root_path}/data-processing/verification/criu_cpuinfo_check.csv')
+    compatible = df[df['compatibility'] == True]
+    incompatible = df[df['compatibility'] == False]
+
+    selected_columns = ['source', 'destination']
+    compatible = compatible[selected_columns]
+    incompatible = incompatible[selected_columns]
+
+    migration_success, migration_failed = readCSV()
+
+    # TN(마이그레이션 실패 예측 및 실제 실패)
+    # FN(마이그레이션 실패 예측 및 실제 성공)
+    true_negative = pd.merge(incompatible, migration_failed, how='outer', on=['source', 'destination'], indicator=True)
+    false_negative = true_negative[true_negative['_merge'] == 'left_only']
+    true_negative = true_negative[true_negative['_merge'] == 'both']
+
+    # TP(마이그레이션 성공 예측 및 실제 성공)
+    # FP(마이그레이션 성공 예측 및 실제 실패)
+    true_positive = pd.merge(compatible, migration_success, how='outer', on=['source', 'destination'], indicator=True)
+    false_positive = true_positive[true_positive['_merge'] == 'left_only']
+    true_positive = true_positive[true_positive['_merge'] == 'both']
+
+    # '_merge' 열 제거
+    if len(true_negative) < 1:
+        true_negative = true_negative.drop(columns=['_merge'])
+    if len(false_negative) < 1:
+        false_negative = false_negative.drop(columns=['_merge'])
+    if len(true_positive) < 1:
+        true_positive = true_positive.drop(columns=['_merge'])
+    if len(false_positive) < 1:
+        false_positive = false_positive.drop(columns=['_merge'])
+    
+    result = dict()
+
+    for instanceType in instances:
+        validate = dict()
+
+        TN = (true_negative['source'] == instanceType).sum()
+        FN = (false_negative['source'] == instanceType).sum()
+        FP = (false_positive['source'] == instanceType).sum()
+        TP = (true_positive['source'] == instanceType).sum()
+
+        validate['TN(마이그레이션 실패 예측 및 실제 실패)'] = TN
+        validate['FN(마이그레이션 실패 예측 및 실제 성공)'] = FN
+        validate['FP(마이그레이션 성공 예측 및 실제 실패)'] = FP
+        validate['TP(마이그레이션 성공 예측 및 실제 성공)'] = TP
+
+        if TP + FN == 0:
+            validate['recall'] = 0
+        else:
+            validate['recall'] = TP / (TP + FN)
+            recall += TP / (TP + FN)
+
+        result[instanceType] = validate
+
+        TN = FN = FP = TP = 0
+
+    pprint(result)
+
+    print(f'total recall : {recall / 27:.3f}')
 
 
 def calTransferableMap(GROUP_NUMBER, df):
@@ -77,7 +144,6 @@ def validateSuccessPrediction(df, transferableGroups, migration_success):
     
     if check:
         print(df)
-    print
 
 
 def validateFailurePrediction(df, transferableGroups, migration_failed):
@@ -103,6 +169,8 @@ def validateFailurePrediction(df, transferableGroups, migration_failed):
 
 def validateForAllInstances():
     result = dict()
+    recall = 0
+
     global truePositive, falsePositive, trueNegative, falseNegative
 
     isa_lookup = GspreadUtils.read_gspread('us-west-2 x86 isa set(23.08.31)')
@@ -134,12 +202,19 @@ def validateForAllInstances():
         validate['FN(마이그레이션 실패 예측 및 실제 성공)'] = falseNegative
         validate['FP(마이그레이션 성공 예측 및 실제 실패)'] = falsePositive
         validate['TP(마이그레이션 성공 예측 및 실제 성공)'] = truePositive
-        validate['recall'] = truePositive / (truePositive + falseNegative)
+
+        if truePositive + falseNegative == 0:
+            validate['recall'] = 0
+        else:
+            validate['recall'] = truePositive / (truePositive + falseNegative)
+            recall += truePositive / (truePositive + falseNegative)
 
         result[instanceType] = validate
         truePositive = falsePositive = trueNegative = falseNegative = 0
 
     pprint(result)
+
+    print(f'total recall : {recall / 27:.3f}')
 
 
 def validateForSpecificInstance(df, transferableGroups, instanceType):
@@ -158,14 +233,5 @@ if __name__ == "__main__":
     trueNegative = 0
     falseNegative = 0
 
+    criu_cpu_info_check()
     validateForAllInstances()
-
-    # df = GspreadUtils.read_gspread('adx(r4.large)')
-    # transferableGroups = calTransferableMap(len(df), df)
-    # validateForSpecificInstance(df, transferableGroups, 'r4.large')
-
-    # print(f'TP(마이그레이션 성공 예측 및 실제 성공) : {truePositive}')
-    # print(f'TN(마이그레이션 실패 예측 및 실제 실패) : {trueNegative}')
-    # print(f'FP(마이그레이션 성공 예측 및 실제 실패) : {falsePositive}')
-    # print(f'FN(마이그레이션 실패 예측 및 실제로 성공) : {falseNegative}')
-    # print(f'recall : {truePositive / (truePositive + falseNegative):.3f}')
