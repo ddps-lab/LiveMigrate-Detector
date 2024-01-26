cbd922b1ffa52a7a8b965add7a299fd6e4a37660
limjunho <jewel7492@gmail.com>
2024-01-26 10:35:14 +0900

    [ADD] lea tracking(include debug code)

--- workload_instruction_analyzer/gdb_script_exepath_tracking.py
+++ workload_instruction_analyzer/gdb_script_exepath_tracking.py
@@ -47,6 +47,22 @@ def get_got_sections():
 
     return got_addr
 
+def get_text_sections():
+    files = gdb.execute("info files", to_string=True)
+
+    lines = files.split('\n')
+
+    sections = []
+
+    for line in lines:
+        # If line contains ".text" save the section addresses
+        # The format is 0x0000564c59c75ae0 - 0x0000564c59f24b9e is .text
+        if ".text" in line:
+            addresses = line.split()
+            sections.append((int(addresses[0], 16), int(addresses[2], 16), ' '.join(addresses[4:])))
+
+    return sections
+
 # glibc는 항상 TSX를 지원하도록 컴파일되지만 TSX는 애플리케이션을 실행하기 전에(예: 를 실행하여 ) 환경 변수가 glibc.elision.enable1로 설정된 경우에만 사용됨.
 # TSX를 사용하도록 환경변수가 설정되지 않았다면 glibc의 내부 함수에서 TSX를 사용하는 __GI___lll_lock_elision, __GI___lll_unlock_elision 를 트래킹에서 제외함.
 def glibc_tunables_check():
@@ -72,6 +88,12 @@ def binding_check(PID):
 def dis_func(addr, tracked_instructions):
     global xtest_enable
     global is_tsx_run
+
+    global logging_functions
+
+    global compile_indirect
+    global runtime_indirect
+    global call_regi
     
     tsx_enabled_glibc_functions = ['__GI___lll_lock_elision', '__GI___lll_unlock_elision']
 
@@ -80,16 +102,21 @@ def dis_func(addr, tracked_instructions):
     transfer_instructions = set(('call', 'jmp', 'ja', 'jnbe', 'jae', 'jnb', 'jb', 'jnae', 'jbe', 'jna', 'jc', 'je', 'jz', 'jnc', 'jne', 'jnz', 
     'jnp', 'jpo', 'jp', 'jpe', 'jcxz', 'jecxz', 'jg', 'jnle', 'jge', 'jnl', 'jl', 'jnge', 'jle', 'jng', 'jno', 'jns', 'jo', 'js'))
 
+    special_instruction = transfer_instructions.copy()
+    special_instruction.add('lea')
+
     try:
         disas_result = gdb.execute(f"disas /r {addr}", to_string=True)
     except gdb.error as e:
         print(f'error: {addr}')
+        return None
 
     lines = disas_result.split('\n')
 
     for line in lines:
         if 'Dump of assembler code for function' in line:
             func_name = line.split('function ')[1].split(':')[0]
+            logging_functions.append(func_name)
 
         # tsx를 사용하는 glibc 내부 함수에 대해 처리
         if func_name in tsx_enabled_glibc_functions:
@@ -111,7 +138,7 @@ def dis_func(addr, tracked_instructions):
         instruction_hex = instruction_part.split('\t')[0].replace(' ', '')
         instruction = instruction_part.split('\t')[1].split(' ')[0]
 
-        if instruction in transfer_instructions:
+        if instruction in special_instruction:
             pass
         elif instruction in tracked_instructions:
             continue
@@ -119,9 +146,17 @@ def dis_func(addr, tracked_instructions):
             tracked_instructions.add(instruction)        
 
         gdb_comment = instruction_part.split(' ')[-1]
+
         # exclude cold functions
         if '-' in gdb_comment or '.cold' in gdb_comment:
             continue
+
+        if 'call' in instruction_part and 'QWORD PTR' in line:
+            if not '#' in line:
+                if '+' in instruction_part:
+                    compile_indirect.append(line)
+                else:
+                    runtime_indirect.append(line)
 
         # ex) "0x0000561161740078 <_start+24>"
         addresse_part = parts[0].strip()
@@ -153,6 +188,18 @@ def dis_func(addr, tracked_instructions):
                 abs_addr = int(gdb_comment, 16)
                 if got_addr[0] <= abs_addr <= got_addr[1]:
                     is_func_call = 'got'
+            # FIXME
+            else:
+                call_regi.append(line)
+        
+        if instruction == 'lea':
+            if gdb_comment.startswith('<'):
+                if '+' in gdb_comment:
+                    pass
+                else:
+                    is_func_call = 'lea'
+                    # 코멘트의 주소를 활용해 주소를 찾는 중복 작업을 제거
+                    gdb_comment = instruction_part.split('#')[1].strip()
 
         if instruction == 'xtest':
             xtest_enable = True
@@ -160,7 +207,6 @@ def dis_func(addr, tracked_instructions):
             is_tsx_run = True
 
         func[instruction_hex] = address, is_func_call, gdb_comment
-    
     return func
 
 def address_calculation(instruction_data, instruction_addr, is_func_call, gdb_comment, tracking_functions):
@@ -174,15 +220,13 @@ def address_calculation(instruction_data, instruction_addr, is_func_call, gdb_co
         # Calculate Call Address
         address = instruction_data["SHORT"].split(' ')
         address = list(filter(None, address))
-        
+
         # QWORD PTR [rip+0x2f53] 처럼 call 하는 주소를 계산할 수 없는 경우
-        if not address[1].startswith('0x'):
-            return None
-        
-        address = int(instruction_data["SHORT"].split(' ')[-1], 16)
-        address = address + instruction_addr
-        address = hex(address & 0xFFFFFFFFFFFFFFFF)  # 결과를 64비트로 자르기
-        address = "0x{:016x}".format(int(address, 16))
+        if is_func_call != 'lea':
+            address = int(instruction_data["SHORT"].split(' ')[-1], 16)
+            address = address + instruction_addr
+            address = hex(address & 0xFFFFFFFFFFFFFFFF)  # 결과를 64비트로 자르기
+            address = "0x{:016x}".format(int(address, 16))
     
     # if address is not function start address 
     if is_func_call == 'plt':
@@ -200,6 +244,10 @@ def address_calculation(instruction_data, instruction_addr, is_func_call, gdb_co
             address = [part for part in address if part.startswith('0x')]
             address = ''.join(address)
 
+    if is_func_call == 'lea':
+        address = gdb_comment.split(' ')[0].strip()
+        address = "0x{:016x}".format(int(address, 16))
+
     if address not in tracking_functions:
         return address
     else:
@@ -214,6 +262,7 @@ def tracking():
 
     # search the starting point of tracking
     start_addr = gdb.execute(f"p/x (long) main", to_string=True).split(' ')[-1]
+    # start_addr = gdb.execute(f"p/x (long) _start", to_string=True).split(' ')[-1]
     start_addr = "0x{:016x}".format(int(start_addr, 16))
 
     tracking_functions.add(start_addr)
@@ -242,15 +291,17 @@ def tracking():
             # Calculate the target address in case of a transfer instruction
             if is_func_call:
                 dst_addr = address_calculation(instruction_data, instruction_addr, is_func_call, gdb_comment, tracking_functions)
+
                 if dst_addr: 
                     tracking_functions.add(dst_addr)
                     list_tracking_functions.append(dst_addr)
             
-    
+    print(f'tracked function count: {len(list_tracking_functions)}')
     utils.create_csv(executable_instructions, is_tsx_run, xtest_enable)
 
 
 if __name__ == '__main__':
+    logging_functions = []
     gdb.execute(f"set pagination off")
     
     # 쉘 스크립트에서 전달된 workload PID 가져오기
@@ -260,7 +311,21 @@ if __name__ == '__main__':
     binding_check(PID)
     
     got_addr = get_got_sections()
+    sections = get_text_sections()
+
+    compile_indirect = []
+    runtime_indirect = []
+    call_regi = []
 
     tracking()
 
-    exit()
+
+    with open('tracked_functions.txt', 'w') as f:
+        f.write('\n'.join(logging_functions))   
+    with open('compile_indirect.txt', 'w') as f:
+        f.write('\n'.join(compile_indirect))   
+    with open('runtime_indirect.txt', 'w') as f:
+        f.write('\n'.join(runtime_indirect))   
+    with open('call_regi.txt', 'w') as f:
+        f.write('\n'.join(call_regi))           
+    # exit()
--- workload_instruction_analyzer/log/disas.py
+++ workload_instruction_analyzer/log/disas.py
@@ -1,7 +1,15 @@
 import gdb
 from pprint import pprint
 
-disas_file = '/home/ubuntu/migration_test/ins_disas/log/disas.txt'
+from pathlib import Path
+import sys
+import os
+
+rootdir = str(Path(__file__).resolve().parent)
+
+disas_file = f'{rootdir}/disas.txt'
+call_regi_file = f'{rootdir}/call_regi.txt'
+address_comment_file = f'{rootdir}/address_comment.txt'
 
 def get_text_sections():
     files = gdb.execute("info files", to_string=True)
@@ -19,7 +27,9 @@ def get_text_sections():
 
     return sections
 
-def disas(start_addr, end_addr, buffered_output, call_regi):
+def disas(start_addr, end_addr, buffered_output, address_comment, call_regi):
+    global sections
+
     transfer_instructions = set(('call', 'jmp', 'ja', 'jnbe', 'jae', 'jnb', 'jb', 'jnae', 'jbe', 'jna', 'jc', 'je', 'jz', 'jnc', 'jne', 'jnz', 
     'jnp', 'jpo', 'jp', 'jpe', 'jcxz', 'jecxz', 'jg', 'jnle', 'jge', 'jnl', 'jl', 'jnge', 'jle', 'jng', 'jno', 'jns', 'jo', 'js'))
     
@@ -28,7 +38,6 @@ def disas(start_addr, end_addr, buffered_output, call_regi):
     lines = disas_result.split('\n')
 
     for line in lines:
-
         parts = line.strip().split(":")
         parts = list(filter(None, parts))
 
@@ -40,16 +49,21 @@ def disas(start_addr, end_addr, buffered_output, call_regi):
         instruction_part = parts[1].strip()
         instruction = instruction_part.split('\t')[1].split(' ')[0]
 
-        gdb_comment = instruction_part.split(' ')[-1]
+        gdb_comment = ''
+        if '#' in line:
+            gdb_comment = instruction_part.split('#')[1].strip()
 
-        # if '-' in line or '.cold' in line:
-        #     continue
+        if '-' in line or '.cold' in line:
+            continue
+
+        if gdb_comment.startswith('0x'):
+            comment_addr = int(gdb_comment.split(' ')[0].strip(), 16)
+
+            for start_addr, end_addr, _ in sections:
+                if comment_addr >= start_addr and comment_addr <= end_addr:
+                    address_comment.append(line)
 
         if instruction in transfer_instructions:       
-            if gdb_comment.startswith('0x'):
-                buffered_output.append(line)
-                continue
-
             if not gdb_comment.startswith('<'):
                 call_regi.append(line)
 
@@ -57,18 +71,24 @@ def disas(start_addr, end_addr, buffered_output, call_regi):
 
 
 if __name__ == '__main__':
+    gdb.execute(f"set pagination off")
+
     sections = get_text_sections()
-    pprint(sections)
+    pprint(sections, width=100)
 
     call_regi = []
     
     seen = set()
     buffered_output = []
+    address_comment = []
     for start_addr, end_addr, name in sections:
-        disas(start_addr, end_addr, buffered_output, call_regi)
+        disas(start_addr, end_addr, buffered_output, address_comment, call_regi)
     
     with open(disas_file, 'w') as f:
         f.write('\n'.join(buffered_output))
 
-    with open('call_regi.txt', 'w') as f:
+    with open(call_regi_file, 'w') as f:
         f.write('\n'.join(call_regi))
+
+    with open(address_comment_file, 'w') as f:
+        f.write('\n'.join(address_comment))    
