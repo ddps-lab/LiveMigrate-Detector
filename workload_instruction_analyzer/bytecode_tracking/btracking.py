faffc4360efb60f4bd5f0d47214df77af9ef2c41
limjunho <jewel7492@gmail.com>
2024-02-01 17:04:46 +0900

    [ADD] User-Defined Objects - Incorporate Class Constructor (__init__) into Class Creation Function

--- workload_instruction_analyzer/bytecode_tracking/btracking.py
+++ workload_instruction_analyzer/bytecode_tracking/btracking.py
@@ -24,6 +24,9 @@ def parse_pychaces():
     return pymodules
 
 def preprocessing_bytecode(byte_code):
+    '''
+    바이트코드를 main과 def 파트로 구분해 각각 반환.
+    '''
     # StringIO 객체를 생성
     f = io.StringIO()
     # dis의 출력을 StringIO 객체로 리디렉션
@@ -51,12 +54,20 @@ def is_builtin_module(module_name):
         return False
 
 def postprocessing_defmap(DEF_MAP, addr_map):
+    '''
+    함수 이름 중복을 구분하기 위해 상위 객체까지 이름에 포함.
+    상위 객체의 주소를 이름으로 치환.
+    '''
     obj_addrs = addr_map.keys()
     def_map = DEF_MAP.copy()
     for key, _ in def_map.items():
         if key.split('.')[0] in obj_addrs:
             parent, value = next(iter(addr_map[key.split('.')[0]].items()))
-            replace = parent + '.' + value
+            # 생성자는 클래스 할당 함수에 포함
+            if value == '__init__':
+                replace = parent
+            else:
+                replace = parent + '.' + value
             DEF_MAP[replace] = DEF_MAP.pop(key)
         else:
             DEF_MAP[key.split('.')[1]] = DEF_MAP.pop(key)
@@ -76,6 +87,9 @@ def postprocessing_bytecode():
             CALL_TRACE[module]['__module_type'] = 'cmodule'
 
 def parse_definition(definitions):
+    '''
+    사용자 정의 객체를 수집.
+    '''
     obj_lists = []
     obj_sets = set()
     for i in range(len(definitions)):
@@ -85,15 +99,26 @@ def parse_definition(definitions):
         obj_lists.append({obj:addr})
 
     return obj_sets, obj_lists
-            
+
+def user_def_tracking(called_map, obj_map, def_map, tracked):
+
+    for obj in called_map['__user_def']:
+        tracked.add(obj)
+        # 클래스 선언 또는 단독 함수 호출
+        if obj in def_map.keys():
+            tracked.update(def_map[obj])
+        else:
+            print(obj)
+    
+    print(tracked)
+
 if __name__ == '__main__':
-    CALL_TRACE = {}
     LIBRARIES = stdlib_list("3.10")
-    modules = {}
-    USER_DEF = {}
-    DEF_MAP = {}
+    called_map = {}
+    def_map = {}
     addr_map = {}
     obj_map = {}
+    tracked = set()
 
     with open('example_scripts/main.py', 'r') as f:
     # with open('example_scripts/testpymodule.py', 'r') as f:
@@ -104,20 +129,20 @@ if __name__ == '__main__':
     # 현재 파싱중인 스크립트에 정의된 객체(함수, 클래스, 메서드)
     obj_sets, user_def_list = parse_definition(definitions)
 
-    print(obj_sets)
-
     for i in range(len(definitions)):
         key, value = next(iter(user_def_list[i].items()))
-        DEF_MAP[value + '.' + key] = bcode_parser.parse_def(definitions[i], addr_map, obj_sets, obj_map)
+        def_map[value + '.' + key] = bcode_parser.parse_def(definitions[i], addr_map, obj_sets, obj_map)
 
     print('------------------------------------------------------------------------------------------------------------')
-    postprocessing_defmap(DEF_MAP, addr_map)
+    postprocessing_defmap(def_map, addr_map)
     print('==== def map ====')
-    pprint(DEF_MAP)
+    pprint(def_map)
     print('------------------------------------------------------------------------------------------------------------')
-    modules = bcode_parser.parse_main(codes, addr_map, obj_sets, obj_map)
+    called_map = bcode_parser.parse_main(codes, addr_map, obj_sets, obj_map)
     print('==== called map ====')
-    pprint(modules)
+    pprint(called_map)
     print('------------------------------------------------------------------------------------------------------------')
     pprint(obj_map)
     print('------------------------------------------------------------------------------------------------------------')
+
+    # user_def_tracking(called_map, obj_map, def_map, tracked)
