87e2fc4ab0ac57157f8a04df092a593fcdc0c3b9
cg10036 <cg10036@gmail.com>
2025-06-23 16:01:49 +0900

    add: model auto download

--- workload_instruction_analyzer/bytecode_tracking/exp_workloads/llm/llm.py
+++ workload_instruction_analyzer/bytecode_tracking/exp_workloads/llm/llm.py
@@ -1,7 +1,19 @@
 from llama_cpp import Llama
 import time
+import os
+import requests
+
+MODEL_PATH = "./Qwen3-0.6B-Q8_0.gguf"
+
+# check model exists
+if not os.path.exists(MODEL_PATH):
+    response = requests.get(
+        "https://huggingface.co/unsloth/Qwen3-0.6B-GGUF/resolve/main/Qwen3-0.6B-Q8_0.gguf?download=true")
+    with open(MODEL_PATH, "wb") as f:
+        f.write(response.content)
+
 llm = Llama(
-    model_path="./Qwen3-0.6B-Q8_0.gguf",
+    model_path=MODEL_PATH,
 )
 
 while True:
