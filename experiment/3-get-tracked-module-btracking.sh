bd07f48c563c4481662edcb5d2bbed39c5ea6ca1
cg10036 <cg10036@gmail.com>
2025-08-08 11:33:01 +0900

    fix

--- experiment/3-get-tracked-module-btracking.sh
+++ experiment/3-get-tracked-module-btracking.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p result/3-get-tracked-module-btracking
+
 test_array=("beautifulsoup4/bs4_example.py" "dask_matmul/dask_matmul.py" "dask_uuid/dask_uuid.py" "falcon_http/falcon_http.py" "fastapi/fastapi_example.py" "int8dot/int8dot_test.py" "llm/llm.py" "matmul/matmul.py" "matplotlib/matplotlib_example.py" "pku/pku_test.py" "rand/rand.py" "rsa/rsa_test.py" "sha/sha_test.py" "sklearn/sklearn_example.py" "xgboost/xgb_example.py")
 
 for test in "${test_array[@]}"; do
